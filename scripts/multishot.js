e624396d7c2a0354983aa0e52a458773fa4f5e3c
Jeroen van Tilburg <jvantilburg@gmail.com>
2023-01-14 12:01:56 +0100

    Remove automatic rotation for iOS
    Is not needed for iOS>=13. For iOS<13 users can manually change the rotation.

--- scripts/multishot.js
+++ scripts/multishot.js
@@ -278,7 +278,7 @@
 
       // Update orientation/rotation
       videoRotation = MI.Get(MediaInfoModule.Stream.Video, 0, 'Rotation');
-      if( iOS() && videoRotation ) {        
+      /*if( iOS() && videoRotation ) {        
         if( Math.abs(90 - videoRotation) < 1 ) {
           $("#orientationInput").val( "90" );
         } else if( Math.abs(180 - videoRotation ) < 1 ) {
@@ -288,7 +288,7 @@
         }
         canvasVideoCtx.save(); // save unrotated state
         rotateContext();
-      }
+      }*/
       
       // Update frame rate
       let frameRate = MI.Get(MediaInfoModule.Stream.Video, 0, 'FrameRate');
